e899a35f99e8c4f3d42ca34a1d5b076b68799ddc
Charuka Rathnayaka <62426265+charuka-rathnayaka@users.noreply.github.com>
2020-06-13 17:22:05 +0530

    Update saveappointment.php

--- Karapitiya_dental/saveappointment.php
+++ Karapitiya_dental/saveappointment.php
@@ -9,9 +9,15 @@ $appointmentnumber = $_POST['appointmentnumber'];
 $treatment = $_POST['treatment'];
 $treatmentDetails = $_POST['treatmentDetails'];
 
-$sql = "INSERT INTO pastappoitment (username,date,ID,Appointmentnumber,category,treatmentdetsils) VALUES ('$username','$date','$ID','$appointmentnumber','$treatment','$treatmentDetails');";
+$sql = "INSERT INTO pastappointment (username,`date`,ID,appointmentnumber,category,treatmentdetsils) VALUES ('$username','$date','$ID','$appointmentnumber','$treatment','$treatmentDetails');";
 mysqli_query($database, $sql);
+$tod=date("Y-m-d");
+$today=date_create($tod);
+$sql_query71="UPDATE `patient_dental_visit_status` SET `Status` = 'not_notified', `Last_appointment` = '$tod' WHERE `patient_dental_visit_status`.`Username` = '$username'";
+mysqli_query($database, $sql_query71);
+
 $sql2 = "DELETE FROM todayappointment WHERE `username`='$username' AND `date`='$date' AND `Appointmentnumber`='$appointmentnumber'";
 mysqli_query($database, $sql2);
 
-header('Location:today_appointments.php');
+/*header('Location:today_appointments.php');*/
+?>
