cf0343b060a09060d7a8c6b76646e63072064366
gowantha <64398901+gowantha@users.noreply.github.com>
2020-06-18 22:23:57 +0530

    Update viewAppointment.php

--- Karapitiya_dental/viewAppointment.php
+++ Karapitiya_dental/viewAppointment.php
@@ -65,10 +65,10 @@ if (empty($_SESSION['username'])) {
     </div>
 
     <div class="navbar">
-        <a href="index.php">Home</a>
+        <a href="index.php"> <img src="icons/Home.svg" class="navBarIcons"> Home</a>
         <div id="part1">
             <div class="dropdown">
-                <button class="dropbtn">Treatments
+                <button class="dropbtn"> <img src="icons/Treat.svg" class="navBarIcons"> Treatments
                     <i class="fa fa-caret-down"></i>
                 </button>
                 <div class="dropdown-content">
@@ -79,7 +79,7 @@ if (empty($_SESSION['username'])) {
         </div>
         <div id="part2">
             <div class="dropdown">
-                <button class="dropbtn">Appointments
+                <button class="dropbtn"> <img src="icons/Appoint.svg" class="navBarIcons"> Appointments
                     <i class="fa fa-caret-down"></i>
                 </button>
                 <div class="dropdown-content">
@@ -88,11 +88,11 @@ if (empty($_SESSION['username'])) {
             </div>
         </div>
         <div id="part3">
-            <a href="my_profile.php">My Profile</a>
+            <a href="my_profile.php"> <img src="icons/Profile.svg" class="navBarIcons"> My Profile</a>
         </div>
         <div id="part4">
-            <a href="about_us.php">About</a>
-            <a href="contact_us.php">Contact</a>
+            <a href="about_us.php"> <img src="icons/About.svg" class="navBarIcons"> About</a>
+            <a href="contact_us.php"> <img src="icons/Contact.svg" class="navBarIcons"> Contact</a>
         </div>
     </div>
     <div class="content">
