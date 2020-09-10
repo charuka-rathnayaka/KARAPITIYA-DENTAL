fecdd99f7e6b98cc7626fcd2f66e6829a04a7733
wishwashan <64398675+wishwashan@users.noreply.github.com>
2020-09-10 11:07:00 +0530

    Add files via upload

--- Karapitiya_dental/appointment.php
+++ Karapitiya_dental/appointment.php
@@ -1,6 +1,7 @@
 <?php
 include_once ('database_server.php');
 include('config.php');
+include('dataLayer.php');
 class Appointment{
 	private $state;
 	public function __construct(){
@@ -38,7 +39,8 @@ abstract class State{
 class CreateState extends State{
 	function create(Appointment $appointment){
 		$array= array();
-		$date= $_POST['date'];
+		$day= $_POST['date'];
+		$date=date("d-m-Y",strtotime($day));
 		$choice=$_POST['choice'];
 		array_push($array,$date);
 		array_push($array,$choice);
@@ -65,48 +67,7 @@ class PendingState extends State{
 	}
 	function pending(Appointment $appointment,$date,$choice){
 		$appointment->setState(new ClosedState());
-		$database= new Db_Connection();
-		$db = $database->connect();
-		$query1="SELECT * FROM booking WHERE date='$date' && category='$choice' && time='8.00-9.00a.m'";
-	$query2="SELECT * FROM booking WHERE date='$date' && category='$choice' && time='9.00-10.00a.m'";
-	$query3="SELECT * FROM booking WHERE date='$date' && category='$choice' && time='10.00-11.00a.m'";
-	$query4="SELECT * FROM booking WHERE date='$date' && category='$choice' && time='11.00-12.00a.m'";
-	$query5="SELECT * FROM booking WHERE date='$date' && category='$choice' && time='12.00-1.00a.m'";
-	$query6="SELECT * FROM booking WHERE date='$date' && category='$choice' && time='1.00-2.00a.m'";
-	$query7="SELECT * FROM booking WHERE date='$date' && category='$choice' && time='2.00-3.00a.m'";
-	$query8="SELECT * FROM booking WHERE date='$date' && category='$choice' && time='3.00-4.00a.m'";
-	if (mysqli_num_rows(mysqli_query($db,$query1)) > 4) {
-  	  $name_error1 = "Sorry1...";
-	  return $name_error1;
-	}
-	if (mysqli_num_rows(mysqli_query($db,$query2)) > 4) {
-  	  $name_error2 = "Sorry2...";
-	  return $name_error2;
-	}
-	if (mysqli_num_rows(mysqli_query($db,$query3)) > 4) {
-  	  $name_error3 = "Sorry3...";
-	  return $name_error3;
-	}
-	if (mysqli_num_rows(mysqli_query($db,$query4)) > 4) {
-  	  $name_error4 = "Sorry4...";
-	  return $name_error4;
-	}
-	if (mysqli_num_rows(mysqli_query($db,$query5)) > 4) {
-  	  $name_error5 = "Sorry5...";
-	  return $name_error5;
-	}
-	if (mysqli_num_rows(mysqli_query($db,$query6)) > 4) {
-  	  $name_error6 = "Sorry6...";
-	  return $name_error6;
-	}
-	if (mysqli_num_rows(mysqli_query($db,$query7)) > 4) {
-  	  $name_error7 = "Sorry7...";
-	  return $name_error7;
-	}
-	if (mysqli_num_rows(mysqli_query($db,$query8)) > 4) {
-  	  $name_error8 = "Sorry8...";
-	  return $name_error8;
-	}
+		return DataLayer::getInstance()->getAppointmentLimit($date,$choice);
 	}
 	function confirm(Appointment $appointment){
 	}
@@ -128,20 +89,7 @@ class ConfirmedState extends State{
 	}
 	function confirm(Appointment $appointment){
 		$appointment->setState(new ClosedState());
-		$database= new Db_Connection();
-		$connection = $database->connect();
-		$patient_name=$_POST['id'];
-		$usrnm=$_SESSION["username"];
-		$select=$_POST['choice'];
-		$day=$_POST['date'];
-		$date=date("d-m-Y",strtotime($day));
-		$time=$_POST['time'];
-		$number=$_POST['number'];
-		$query="Insert into booking(Appointmentnumber,patient_name,category,date,time,username) values('$number','$patient_name','$select','$date','$time','$usrnm')";
-		$querynew="UPDATE `patient_dental_visit_status` SET `Status` = 'not_notified' WHERE `patient_dental_visit_status`.`Username` = '$usrnm'";
-		mysqli_query($connection,$query);
-		mysqli_query($connection,$querynew);
-		mysqli_close($connection);
+		DataLayer::getInstance()->insertData();
 	}
 	function delete(Appointment $appointment){
 		/*echo "Appointment closed";*/
@@ -180,17 +128,10 @@ class DeleteState extends State{
 	function confirm(Appointment $appointment){
 	}
 	function delete(Appointment $appointment){
-		$database= new Db_Connection();
-		$connection= $database->connect();
-		$username= $_SESSION['username'];
-		$date= $_POST['date'];
-		$category=$_POST['treatment'];
-		$number=$_POST['app_num']; 
-		$query="delete from booking where username='$username' and date='$date' and category='$category' and Appointmentnumber='$number'";
-		mysqli_query($connection,$query);
-		mysqli_close($connection);
+		DataLayer::getInstance()->deleteData();
 	}
 	function close(Appointment $appointment){
 		/*echo "Appointment closed";*/
 	}
 }
+?>
--- Karapitiya_dental/check.php
+++ Karapitiya_dental/check.php
@@ -1,6 +1,8 @@
+<!DOCTYPE html>
 <html>
 <head>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+<script src="blogic.js"></script>
 <style>
 .button1{
 	background-color:#F03;
@@ -11,9 +13,16 @@
 <?php 
 include('appointment.php');
 if(isset($_POST['check'])){
-	$app = new Appointment();
-	$array = $app->createAppointment();
-	$error=$app->pendingAppointment($array[0],$array[1]);
+	$day= $_POST['date'];
+	$today= date("Y-m-d");
+	$diff= strtotime($day)-strtotime($today);
+	if(($_POST['name']!=null)&& ($_POST['date']!=null)&& ($diff<=604800 && $diff>0)){
+		$app = new Appointment();
+		$array = $app->createAppointment();
+		$error=$app->pendingAppointment($array[0],$array[1]);
+	}else{
+		$formerro='error';
+	}
 }
 ?>
 <script>
@@ -48,16 +57,16 @@ $(document).ready(function() {
 	});
 });
 </script>
-<?php if(isset($_POST['check'])):?>
+<?php if(isset($_POST['check'])&& !(isset($formerro))):?>
 <h3><font color="#3300FF">Select your free time slot</font></h3>
-<input type="button" name="time" id="timebtn" value="8.00-9.00a.m"<?php if($error=="Sorry1..."):?> class="button1" onClick="warning()"<?php else:?>onClick="toogle(this)" <?php endif?>/>
-<input type="button" name="time" id="timebtn" value="9.00-10.00a.m"<?php if($error=="Sorry2..."):?> class="button1" onClick="warning()"<?php else:?>onClick="toogle(this)" <?php endif?>/>
-<input type="button" name="time" id="timebtn" value="10.00-11.00a.m"<?php if($error=="Sorry3..."):?> class="button1" onClick="warning()"<?php else:?>onClick="toogle(this)" <?php endif?>/>
-<input type="button" name="time" id="timebtn" value="11.00-12.00a.m"<?php if($error=="Sorry4..."):?> class="button1" onClick="warning()"<?php else:?>onClick="toogle(this)" <?php endif?>/><br><br>
-<input type="button" name="time" id="timebtn" value="12.00-1.00p.m"<?php if($error=="Sorry5..."):?> class="button1" onClick="warning()"<?php else:?>onClick="toogle(this)" <?php endif?>/>
-<input type="button" name="time" id="timebtn" value="1.00-2.00p.m"<?php if($error=="Sorry6..."):?> class="button1" onClick="warning()"<?php else:?>onClick="toogle(this)" <?php endif?>/>
-<input type="button" name="time" id="timebtn" value="2.00-3.00p.m"<?php if($error=="Sorry7..."):?> class="button1" onClick="warning()"<?php else:?>onClick="toogle(this)" <?php endif?>/>
-<input type="button" name="time" id="timebtn" value="3.00-4.00p.m"<?php if($error=="Sorry8..."):?> class="button1" onClick="warning()"<?php else:?>onClick="toogle(this)" <?php endif?>/>
+<input type="button" name="time" id="timebtn" value="8.00-9.00a.m"<?php if(in_array("Sorry1...",$error,TRUE)):?> class="button1" onClick="warning()"<?php else:?>onClick="toogle(this)" <?php endif?>/>
+<input type="button" name="time" id="timebtn" value="9.00-10.00a.m"<?php if(in_array("Sorry2...",$error,TRUE)):?> class="button1" onClick="warning()"<?php else:?>onClick="toogle(this)" <?php endif?>/>
+<input type="button" name="time" id="timebtn" value="10.00-11.00a.m"<?php if(in_array("Sorry3...",$error,TRUE)):?> class="button1" onClick="warning()"<?php else:?>onClick="toogle(this)" <?php endif?>/>
+<input type="button" name="time" id="timebtn" value="11.00-12.00a.m"<?php if(in_array("Sorry4...",$error,TRUE)):?> class="button1" onClick="warning()"<?php else:?>onClick="toogle(this)" <?php endif?>/><br><br>
+<input type="button" name="time" id="timebtn" value="12.00-1.00p.m"<?php if(in_array("Sorry5...",$error,TRUE)):?> class="button1" onClick="warning()"<?php else:?>onClick="toogle(this)" <?php endif?>/>
+<input type="button" name="time" id="timebtn" value="1.00-2.00p.m"<?php if(in_array("Sorry6...",$error,TRUE)):?> class="button1" onClick="warning()"<?php else:?>onClick="toogle(this)" <?php endif?>/>
+<input type="button" name="time" id="timebtn" value="2.00-3.00p.m"<?php if(in_array("Sorry7...",$error,TRUE)):?> class="button1" onClick="warning()"<?php else:?>onClick="toogle(this)" <?php endif?>/>
+<input type="button" name="time" id="timebtn" value="3.00-4.00p.m"<?php if(in_array("Sorry8...",$error,TRUE)):?> class="button1" onClick="warning()"<?php else:?>onClick="toogle(this)" <?php endif?>/>
 <?php endif?>
 </body>
 </html>
