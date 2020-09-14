b111bc9143465ff763501d4f24ac075b70c2fe3c
pubuduprabhathiya <64272083+pubuduprabhathiya@users.noreply.github.com>
2020-09-14 18:18:57 +0530

    Add files via upload

---
+++ Karapitiya_dental (1)/registration1.php
@@ -0,0 +1,63 @@
+<?php include("dbOperations/config.php");
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title> User registration form </title>
+    <link rel="stylesheet" type="text/css" href="stylesheet_form.css">
+    <script src="validate_register.js"></script>
+</head>
+
+<body>
+    <div class="header">
+        <h1>Register</h1>
+
+    </div>
+    <form name="reg1" method="POST" action="registration1.php" onsubmit="return validate()">
+        <?php include("errors.php");
+        ?>
+
+        <div class="input-group">
+            <label>Firstname</label>
+            <span id="error_firstname" style="color:red;"></span>
+            <input type="text" name="Firstname" value="<?php  ?>">
+        </div>
+        <div class="input-group">
+            <label>Lastname</label>
+            <span id="error_lastname" style="color:red;"></span>
+            <input type="text" name="Lastname" value="<?php  ?>">
+        </div>
+        <div class="input-group">
+            <label>Email</label>
+            <span id="error_email" style="color:red;"></span>
+            <input type="text" name="Email" value="<?php  ?>">
+        </div>
+        <div class="input-group">
+            <label>Birthday:</label>
+            <span id="error_birthday" style="color:red;"></span>
+            <input type="date" name="Birthday">
+        </div>
+
+        <div class="input-data">
+            <span id="error_gender" style="color:red;"></span>
+            <br>
+            <input type="radio" name="Gender" value="male">
+            <label>Male</label><br>
+            <input type="radio" name="Gender" value="female">
+            <label>Female</label><br>
+        </div>
+        <br>
+        <div class="input-group">
+            <button type="submit" name="Next" class="btn">Next</button>
+        </div>
+        <p>
+            Already a member? <a href="login.php">Sign in</a>
+        </p>
+
+
+    </form>
+
+</body>
+
+</html>
---
+++ Karapitiya_dental (1)/viewAppointment.php
@@ -0,0 +1,141 @@
+<?php include("dbOperations/config.php");
+include_once('dbOperations/memento.php');
+include('dbOperations/waitingListUpdate.php');
+if (empty($_SESSION['username'])) {
+    header("location:login.php");
+} ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Home</title>
+    <link rel="stylesheet" type="text/css" href=stylesheet_view_appointment.css>
+    <link rel="stylesheet" type="text/css" href=stylesheet_home.css>
+    <script src="jquery.min.js">
+
+    </script>
+</head>
+
+<body>
+
+    <div class="topHead">
+        <div class="header">
+            <img src="icons/Dental Unit Logo.svg" class="mainLogo">
+        </div>
+        <div class="content" id="logInBox">
+            <?php if (isset($_SESSION["success"])) : ?>
+                <div class="success">
+                    <h3>
+                        <?php
+
+                        echo $_SESSION["success"];
+                        //echo $_SESSION["user_type"];
+                        unset($_SESSION["success"]);
+                        ?>
+                    </h3>
+                </div>
+            <?php endif ?>
+            <?php
+            if (isset($_SESSION['username'])) :
+
+            ?>
+                <div class="welcome">
+                    <p>WELCOME <strong> <?php echo $_SESSION['username']; ?></strong></p>
+                    <div class="log"> <a href='index.php?logout=' 1' ' > <img src="icons/Logout_black.svg" class="navBarIcons"> Logout</a></div>
+                    </div>
+                
+                    <?php endif ?>
+                    <?php
+                    if (!isset($_SESSION['username'])) : ?>
+                    <div class="welcome">
+                        	 <div class="log"> <a href=' login.php'> <img src="icons/Login_black.svg" class="navBarIcons"> Login</a></div>
+                </div>
+            <?php endif ?>
+
+        </div>
+    </div>
+    </div>
+
+    <div class="navbar">
+        <a href="index.php"> <img src="icons/Home.svg" class="navBarIcons"> Home</a>
+        <div id="part1">
+            <div class="dropdown">
+                <button class="dropbtn"> <img src="icons/Treat.svg" class="navBarIcons"> Treatments
+                    <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                    <a href="basic_treatments.php">Basic Treatments</a>
+                    <a href="advance_treatments.php">Advance Treatments</a>
+                </div>
+            </div>
+        </div>
+        <div id="part2">
+            <div class="dropdown">
+                <button class="dropbtn"> <img src="icons/Appoint.svg" class="navBarIcons"> Appointments
+                    <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                    <a href="add_new_appointment.php">Make new Appointment</a>
+                    <a href="view_my_appointment.php">View My Appointments</a>
+                </div>
+            </div>
+        </div>
+        <div id="doctorPerspective">
+            <a href="today_appointments.php"> <img src="icons/About.svg" class="navBarIcons"> View Appointments </a>
+            <a href="profile_doctor.php"> <img src="icons/Profile.svg" class="navBarIcons"> My Profile </a>
+            <a href="personal_blog.php"> <img src="icons/About.svg" class="navBarIcons"> Blog </a>
+        </div>
+        <div id="adminPerspective">
+            <a href="add_doctor.php"> <img src="icons/About.svg" class="navBarIcons"> Add Doctor </a>
+        </div>
+        <div id="staffPerspective">
+            <a href="add_new_appointment.php"> <img src="icons/About.svg" class="navBarIcons"> Add Appointments </a>
+            <a href="viewAppointment.php"> <img src="icons/Profile.svg" class="navBarIcons"> View Appointments </a>
+        </div>
+        <div id="part3">
+            <a href="my_profile.php"> <img src="icons/Profile.svg" class="navBarIcons"> My Profile</a>
+        </div>
+        <div id="part4">
+            <a href="about_us.php"> <img src="icons/About.svg" class="navBarIcons"> About</a>
+            <a href="contact_us.php"> <img src="icons/Contact.svg" class="navBarIcons"> Contact</a>
+        </div>
+    </div>
+
+    <?php include('user_type_menu.php');
+    ?>
+
+    <h2>APPOINTMENTS - TODAY</h2>
+    <div>
+        <?php
+
+        $ori = new Originato();
+        $cre = new Caretaker($ori);
+
+        if (array_key_exists('button1', $_POST)) {
+            $cre->undo();
+        }
+
+
+        ?>
+
+        <form method="post">
+            <input type="submit" name="button1" class="button" value="BACK" />
+
+
+        </form>
+
+
+
+    </div>
+
+
+    <div id="tables">
+        <?php
+        include('showTables.php');
+        ?>
+
+    </div>
+
+</body>
+
+</html>
