9f6105797b985b206a5d131cca2c8a92c09e7d85
wishwashan <64398675+wishwashan@users.noreply.github.com>
2020-09-17 09:23:07 +0530

    Add files via upload

--- Karapitiya_dental (1)/add_new_appointment.php
+++ Karapitiya_dental (1)/add_new_appointment.php
@@ -28,94 +28,96 @@ if (empty($_SESSION['username'])) {
 </head>
 
 <body>
-        <div class="topHead">
-                <div class="header">
-                    <img src="icons/Dental Unit Logo.svg" class="mainLogo">
-                </div>
-                <div class="cont" id="logInBox">
-                    <?php if(isset($_SESSION["success"])):?>
-                        <div class="success">
-                            <h3>
-                                <?php
-                                
-                                echo $_SESSION["success"];
-                            //echo $_SESSION["user_type"];
-                                unset($_SESSION["success"]);
-                                ?>
-                            </h3>
-                        </div>
-                        <?php endif ?>
-                    <?php 
-                        if (isset($_SESSION['username'])):
-                        
+    <div class="topHead">
+        <div class="header">
+            <img src="icons/Dental Unit Logo.svg" class="mainLogo">
+        </div>
+        <div class="content" id="logInBox">
+            <?php if (isset($_SESSION["success"])) : ?>
+                <div class="success">
+                    <h3>
+                        <?php
+
+                        echo $_SESSION["success"];
+                        //echo $_SESSION["user_type"];
+                        unset($_SESSION["success"]);
                         ?>
-                        <div class="welcome">
-                            <p>WELCOME <strong> <?php echo $_SESSION['username']; ?></strong></p>
-                            <div class="log"> <a href='index.php?logout='1' ' > <img src="icons/Logout_black.svg" class="navBarIcons"> Logout</a></div>
-                        </div>
-                    
-                        <?php endif ?>
-                        <?php 
-                        if(!isset($_SESSION['username'])): ?>
-                        <div class="welcome">
-                            <div class="log"> <a href='login.php' > <img src="icons/Login_black.svg" class="navBarIcons"> Login</a></div>
-                        </div>
-                        <?php endif ?>
-                        
+                    </h3>
+                </div>
+            <?php endif ?>
+            <?php
+            if (isset($_SESSION['username'])) :
+
+            ?>
+                <div class="welcome">
+                    <p>WELCOME <strong> <?php echo $_SESSION['username']; ?></strong></p>
+                    <div class="log"> <a href='index.php?logout=' 1' ' > <img src="icons/Logout_black.svg" class="navBarIcons"> Logout</a></div>
                     </div>
+                
+                    <?php endif ?>
+                    <?php
+                    if (!isset($_SESSION['username'])) : ?>
+                    <div class="welcome">
+                        <div class="log"> <a href=' login.php'> <img src="icons/Login_black.svg" class="navBarIcons"> Login</a></div>
                 </div>
-            <div class="navbar">
-                <a href="index.php"> <img src="icons/Home.svg" class="navBarIcons"> Home</a>
-                <div id="part1">
-                    <div class="dropdown">
-                        <button class="dropbtn"> <img src="icons/Treat.svg" class="navBarIcons"> Treatments
-                            <i class="fa fa-caret-down"></i>
-                        </button>
-                        <div class="dropdown-content">
-                            <a href="basic_treatments.php">Basic Treatments</a>
-                            <a href="advance_treatments.php">Advance Treatments</a>  
-                        </div> 
-                    </div>
-                </div>
-                <div id="part2">
-                    <div class="dropdown">
-                        <button class="dropbtn"> <img src="icons/Appoint.svg" class="navBarIcons"> Appointments
-                            <i class="fa fa-caret-down"></i>
-                        </button>
-                        <div class="dropdown-content">
-                            <a href="add_new_appointment.php">Make new Appointment</a>
-                            <a href="view_my_appointment.php">View My Appointments</a>   
-                        </div>
-                    </div>
-                </div>
-                <div id="doctorPerspective">
-                    <a href="today_appointments.php"> <img src="icons/Appoint.svg" class="navBarIcons"> View Appointments </a>
-                    <a href="profile_doctor.php"> <img src="icons/Doctor Profile.svg" class="navBarIcons"> My Profile </a>
-                    <a href="personal_blog.php"> <img src="icons/Blog.svg" class="navBarIcons"> Blog </a>
-                </div>
-                <div id="adminPerspective">
-                    <a href="add_doctor.php"> <img src="icons/Add Doctor.svg" class="navBarIcons"> Add Doctor </a>
-                </div>
-                <div id="staffPerspective">
-                    <a href="add_new_appointment.php"> <img src="icons/Add Appointments.svg" class="navBarIcons"> Add Appointments </a>
-                    <a href="viewAppointment.php"> <img src="icons/Appoint.svg" class="navBarIcons"> View Appointments </a>
-                </div>
-                <div id="part3">
-                    <a href="my_profile.php"> <img src="icons/Profile.svg" class="navBarIcons"> My Profile</a>
-                </div>
-                <div id="part4">
-                    <a href="about_us.php"> <img src="icons/About.svg" class="navBarIcons"> About</a>
-                    <a href="contact_us.php"> <img src="icons/Contact.svg" class="navBarIcons"> Contact</a>
+            <?php endif ?>
+
+        </div>
+    </div>
+    </div>
+
+    <div class="navbar">
+        <a href="index.php"> <img src="icons/Home.svg" class="navBarIcons"> Home</a>
+        <div id="part1">
+            <div class="dropdown">
+                <button class="dropbtn"> <img src="icons/Treat.svg" class="navBarIcons"> Treatments
+                    <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                    <a href="basic_treatments.php">Basic Treatments</a>
+                    <a href="advance_treatments.php">Advance Treatments</a>
                 </div>
             </div>
-                
-            <?php include('user_type_menu.php');
-            ?> 
+        </div>
+        <div id="part2">
+            <div class="dropdown">
+                <button class="dropbtn"> <img src="icons/Appoint.svg" class="navBarIcons"> Appointments
+                    <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                    <a href="add_new_appointment.php">Make new Appointment</a>
+                    <a href="view_my_appointment.php">View My Appointments</a>
+                </div>
+            </div>
+        </div>
+        <div id="doctorPerspective">
+            <a href="today_appointments.php"> <img src="icons/About.svg" class="navBarIcons"> View Appointments </a>
+            <a href="profile_doctor.php"> <img src="icons/Profile.svg" class="navBarIcons"> My Profile </a>
+            <a href="personal_blog.php"> <img src="icons/About.svg" class="navBarIcons"> Blog </a>
+        </div>
+        <div id="adminPerspective">
+            <a href="add_doctor.php"> <img src="icons/About.svg" class="navBarIcons"> Add Doctor </a>
+        </div>
+        <div id="staffPerspective">
+            <a href="add_new_appointment.php"> <img src="icons/About.svg" class="navBarIcons"> Add Appointments </a>
+            <a href="viewAppointment.php"> <img src="icons/Profile.svg" class="navBarIcons"> View Appointments </a>
+        </div>
+        <div id="part3">
+            <a href="my_profile.php"> <img src="icons/Profile.svg" class="navBarIcons"> My Profile</a>
+        </div>
+        <div id="part4">
+            <a href="about_us.php"> <img src="icons/About.svg" class="navBarIcons"> About</a>
+            <a href="contact_us.php"> <img src="icons/Contact.svg" class="navBarIcons"> Contact</a>
+        </div>
+    </div>
+
+    <?php include('user_type_menu.php');
+    ?>
 
 
 
     <form action="" method="post">
-        <table class="insertInfo" align="center" cellpadding="5" cellspacing="10">
+        <table align="center" cellpadding="5" cellspacing="10">
             <tr>
                 <td>Patient Name</td>
                 <td>
@@ -127,14 +129,14 @@ if (empty($_SESSION['username'])) {
             <tr>
                 <td>Category</td>
                 <td><select name="choice" id="select1">
-                        <option value="Orthodontics">Bridges</option>
-                        <option value="Orthodontics">Crowns</option>
-                        <option value="Orthodontics">Dental Implants</option>
-                        <option value="Orthodontics">Orthodontics</option>
-                        <option value="NurveFilling">Root canal treatment</option>
-                        <option value="Implants">Scale and polish</option>
-                        <option value="Orthodontics">Teeth whitening</option>
-                        <option value="Orthodontics">Tooth removal</option>
+                        <option value="Bridges">Bridges</option>
+                        <option value="Crowns">Crowns</option>
+                        <option value="Dental Implants">Dental Implants</option>
+              
+                        <option value="Root canal treatment">Root canal treatment</option>
+                        <option value="Scale and polish">Scale and polish</option>
+                        <option value="Teeth whitening">Teeth whitening</option>
+                        <option value="Tooth removal">Tooth removal</option>
 
 
 
@@ -149,7 +151,7 @@ if (empty($_SESSION['username'])) {
                 </td>
             </tr>
             <tr>
-                <td><input type="button" class="checkBtn" name="check" id="check1" value="Check" onClick="send()" /></td>
+                <td><input type="button" name="check" id="check1" value="Check" onClick="send()" /></td>
             </tr>
         </table>
         <div class="bottom"></div>
